674e5f24d04249c1af5b66a77d1e7b54810922a4
rahe3431 <rahe3431@gmail.com>
2024-08-09 11:56:36 -0600

    virtual network added

--- main.tf
+++ main.tf
@@ -18,3 +18,14 @@ resource "azurerm_resource_group" "mtc-rg" {
     environment = "dev"
   }
 }
+
+resource "azurerm_virtual_network" "mtc-vm" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
