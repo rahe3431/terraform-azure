4106253074a211b8198c6f705cc9dbbda56141c8
rahe3431 <rahe3431@gmail.com>
2024-08-09 11:07:02 -0600

    format

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "2.15.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.15.0"
     }
+  }
 }
 
 provider "azurerm" {
-    feature {}
+  feature {}
 }
